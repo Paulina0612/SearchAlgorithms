f24cb52432950df18162e10f9c35d83f512262d3
Paulina Bartczak <paulinabartczak2@onet.pl>
2026-06-30 23:34:23 +0200

    Basic structure of the code implemented (menu and basic methods).

--- SearchAlgorithms/Program.cs
+++ SearchAlgorithms/Program.cs
@@ -1,1 +1,48 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+void Menu()
+{
+    Console.WriteLine("Menu");
+    Console.WriteLine("1. Linear Search.");
+    Console.WriteLine("2. Binary Search.");
+    Console.WriteLine("3. Exit.\n");
+}
+
+
+
+void LinearSearch()
+{
+    Console.WriteLine("Linear");
+}
+
+
+
+void BinarySearch()
+{
+    Console.WriteLine("Binary");
+}
+
+
+
+while (true)
+{
+    Menu();
+    Console.WriteLine("Enter number of the desired option.");
+    var opt = Console.ReadLine();
+
+    switch (opt)
+    {
+        case "1":
+            LinearSearch();
+            break;
+        case "2":
+            BinarySearch();
+            break;
+        case "3":
+            return 0;
+        default:
+            Console.WriteLine("Incorrect input. ");
+            break;
+    }
+
+    Console.Write("\n\n\n");
+}
