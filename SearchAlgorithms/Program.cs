2ee474163c20a79158f42edf4b9d1e361db1a6db
Paulina Bartczak <paulinabartczak2@onet.pl>
2026-07-05 16:03:30 +0200

    Algorithms implemented.

--- SearchAlgorithms/Program.cs
+++ SearchAlgorithms/Program.cs
@@ -9,20 +9,52 @@ void Menu()
 
 
 
-void LinearSearch()
+bool LinearSearch(int[] array, int target)
 {
-    Console.WriteLine("Linear");
+    foreach (int i in array)
+        if (i == target) return true;
+    return false;
 }
 
 
 
-void BinarySearch()
+bool BinarySearch(int[] array, int target)
 {
-    Console.WriteLine("Binary");
+    int start = 0;
+    int end = array.Length-1;
+    int index = end/2;
+
+    while (true)
+    {
+        if (Math.Abs(end - start) == 1 && !(array[start] == target || array[end] == target)) return false;
+
+        if (array[index] == target || array[index + 1] == target) return true;
+        else if (array[index] < target)
+        {
+            start = index;
+            index = ((end - start) / 2) + start;
+        }
+        else if (array[index] > target)
+        {
+            end = index;
+            index = (end - start) / 2;
+        }
+    }
 }
 
 
 
+
+
+// Mockup data
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+int target = 9;
+
+
+
+
+
+// Main loop
 while (true)
 {
     Menu();
@@ -32,13 +64,15 @@ while (true)
     switch (opt)
     {
         case "1":
-            LinearSearch();
+            if (LinearSearch(array, target) is true) Console.WriteLine("Target found.");
+            else Console.WriteLine("Target not found.");
             break;
         case "2":
-            BinarySearch();
+            if (BinarySearch(array, target) == true) Console.WriteLine("Target found.");
+            else Console.WriteLine("Target not found.");
             break;
         case "3":
-            return 0;
+            return 0; 
         default:
             Console.WriteLine("Incorrect input. ");
             break;
